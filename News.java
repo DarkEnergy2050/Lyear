0ab336b7a0c7d48e7dacf68a0029c75784adfd4f
DarkEnergy2050 <276050037.com>
2019-05-24 22:24:12 +0800

    cc

---
+++ News.java
@@ -0,0 +1,29 @@
+package activitytest.example.com.testnew;
+
+/**
+ * Created by Administrator on 2019/5/6.
+ */
+
+public class News {
+  private   String title;
+   private String content;
+    public String  gettitle()
+    {
+        return  title;
+    }
+    public  String  getcontent()
+    {
+        return  content;
+    }
+
+    public void settitle(String title)
+    {
+        this.title=title;
+    }
+    public  void setcontent(String content)
+    {
+        this.content=content;
+    }
+
+
+}
